1771f95792e784123ec9bc6e74fabe1e95d19fab
Muhammad Umar <m.umar@partner.samsung.com>
2018-06-14 19:19:00 +0100

    Simple Layout Example
    Change-Id: I72fa76ce36c979fcfbbbfd13a2b87b7cd41b56db

---
+++ simple-layout/SimpleLayout.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Tizen.NUI;
+using Tizen.NUI.BaseComponents;
+
+namespace SimpleLayout
+{
+    static class TestImages
+    {
+        private const string resources = "./res";
+
+        /// Child image filenames
+        public static readonly string[] s_images = new string[]
+        {
+            resources + "/images/application-icon-101.png",
+            resources + "/images/application-icon-102.png",
+            resources + "/images/application-icon-103.png",
+            resources + "/images/application-icon-104.png"
+        };
+    }
+
+    class SimpleLayout : NUIApplication
+    {
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            // Change the background color of Window to White
+            Window window = Window.Instance;
+            window.BackgroundColor = Color.White;
+
+            // Create a new view
+            View view = new View();
+            view.ParentOrigin = ParentOrigin.Center;
+            view.PivotPoint = PivotPoint.Center;
+            view.PositionUsesPivotPoint = true;
+            window.Add( view);
+
+            // Set our Custom Layout on the view
+            var layout = new CustomLayout();
+            view.Layout = layout;
+
+            // Add child image-views to the created view
+            foreach (String image in TestImages.s_images)
+            {
+                view.Add( CreateChildImageView( image, new Size2D( 100, 100 ) ) );
+            }
+        }
+
+        /// <summary>
+        /// Helper function to create ImageViews with given filename and size..<br />
+        /// </summary>
+        /// <param name="filename"> The filename of the image to use.</param>
+        /// <param name="size"> The size that the image should be loaded at.</param>
+        /// <returns>The created ImageView.</returns>
+        ImageView CreateChildImageView( String url, Size2D size )
+        {
+            ImageView imageView = new ImageView();
+            ImageVisual imageVisual = new ImageVisual();
+
+            imageVisual.URL = url;
+            imageVisual.DesiredHeight = size.Height;
+            imageVisual.DesiredWidth = size.Width;
+            imageView.Image = imageVisual.OutputVisualMap;
+
+            imageView.Name = "ImageView";
+            imageView.PivotPoint = PivotPoint.TopLeft;
+            imageView.PositionUsesPivotPoint = true;
+            imageView.HeightResizePolicy = ResizePolicyType.Fixed;
+            imageView.WidthResizePolicy = ResizePolicyType.Fixed;
+            return imageView;
+        }
+
+        static void Main(string[] args)
+        {
+            SimpleLayout simpleLayout = new SimpleLayout();
+            simpleLayout.Run(args);
+        }
+    }
+}
