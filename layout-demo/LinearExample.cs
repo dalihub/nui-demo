6b7c18da85eacde401dffc9f6576237ed925d762
Anton Obzhirov <a.obzhirov@samsung.com>
2018-09-13 14:15:02 +0100

    Add weight and alignment buttons in NUI LinearLayout example.
    Change-Id: Idc1807e6b2c534ba400c0719813b457a7d39797f

--- layout-demo/LinearExample.cs
+++ layout-demo/LinearExample.cs
@@ -41,6 +41,7 @@ namespace LayoutDemo
             var layout = new LinearLayout();
             view.Layout = layout;
             view.LayoutDirection = ViewLayoutDirectionType.LTR;
+            layout.SetAlignment( LinearLayout.Alignment.Begin );
 
             // Add child image-views to the created view
             foreach (String image in TestImages.s_images)
@@ -56,7 +57,7 @@ namespace LayoutDemo
             PushButton directionButton = new PushButton();
             LayoutingExample.SetUnselectedIcon(directionButton, "./res/images/icon-reverse.png");
             LayoutingExample.SetSelectedIcon(directionButton, "./res/images/icon-reverse-selected.png");
-            directionButton.ParentOrigin = new Vector3(0.33f, 1.0f, 0.5f);
+            directionButton.ParentOrigin = new Vector3(0.2f, 1.0f, 0.5f);
             directionButton.PivotPoint = PivotPoint.BottomCenter;
             directionButton.PositionUsesPivotPoint = true;
             directionButton.MinimumSize = new Vector2(75, 75);
@@ -79,10 +80,49 @@ namespace LayoutDemo
             window.Add(directionButton);
             buttons.Add(directionButton);
 
+            PushButton alignmentButton = new PushButton();
+            LayoutingExample.SetUnselectedIcon(alignmentButton, "./res/images/icon-replace.png");
+            LayoutingExample.SetSelectedIcon(alignmentButton, "./res/images/icon-replace-selected.png");
+            alignmentButton.ParentOrigin = new Vector3(0.4f, 1.0f, 0.5f);
+            alignmentButton.PivotPoint = PivotPoint.BottomCenter;
+            alignmentButton.PositionUsesPivotPoint = true;
+            alignmentButton.MinimumSize = new Vector2(75, 75);
+            alignmentButton.Clicked += (sender, e) =>
+            {
+                LinearLayout linearLayout = (LinearLayout)this.view.Layout;
+                if ( linearLayout.GetAlignment() == LinearLayout.Alignment.Begin )
+                {
+                    linearLayout.SetAlignment( LinearLayout.Alignment.CenterHorizontal );
+                }
+                else if ( linearLayout.GetAlignment() == LinearLayout.Alignment.CenterHorizontal )
+                {
+                    linearLayout.SetAlignment( LinearLayout.Alignment.End );
+                }
+                else if ( linearLayout.GetAlignment() == LinearLayout.Alignment.End )
+                {
+                    linearLayout.SetAlignment( LinearLayout.Alignment.Top );
+                }
+                else if ( linearLayout.GetAlignment() == LinearLayout.Alignment.Top )
+                {
+                    linearLayout.SetAlignment( LinearLayout.Alignment.CenterVertical );
+                }
+                else if ( linearLayout.GetAlignment() == LinearLayout.Alignment.CenterVertical )
+                {
+                    linearLayout.SetAlignment( LinearLayout.Alignment.Bottom );
+                }
+                else if ( linearLayout.GetAlignment() == LinearLayout.Alignment.Bottom )
+                {
+                    linearLayout.SetAlignment( LinearLayout.Alignment.Begin );
+                }
+                return true;
+            };
+            window.Add(alignmentButton);
+            buttons.Add(alignmentButton);
+
             PushButton rotateButton = new PushButton();
             LayoutingExample.SetUnselectedIcon(rotateButton, "./res/images/icon-reset.png");
             LayoutingExample.SetSelectedIcon(rotateButton, "./res/images/icon-reset-selected.png");
-            rotateButton.ParentOrigin = new Vector3(0.66f, 1.0f, 0.5f);
+            rotateButton.ParentOrigin = new Vector3(0.6f, 1.0f, 0.5f);
             rotateButton.PivotPoint = PivotPoint.BottomCenter;
             rotateButton.PositionUsesPivotPoint = true;
             rotateButton.MinimumSize = new Vector2(75, 75);
@@ -99,9 +139,34 @@ namespace LayoutDemo
                 }
                 return true;
             };
-
             window.Add(rotateButton);
             buttons.Add(rotateButton);
+
+            PushButton weightButton = new PushButton();
+            LayoutingExample.SetUnselectedIcon(weightButton, "./res/images/icon-item-view-layout-grid.png");
+            LayoutingExample.SetSelectedIcon(weightButton, "./res/images/icon-item-view-layout-grid-selected.png");
+            weightButton.ParentOrigin = new Vector3(0.8f, 1.0f, 0.5f);
+            weightButton.PivotPoint = PivotPoint.BottomCenter;
+            weightButton.PositionUsesPivotPoint = true;
+            weightButton.MinimumSize = new Vector2(75, 75);
+            weightButton.Clicked += (sender, e) =>
+            {
+                foreach (View child in view.Children)
+                {
+                    if (child.Weight == 0.0f)
+                    {
+                        child.Weight = 0.25f;
+                    }
+                    else
+                    {
+                        child.Weight = 0.0f;
+                    }
+                }
+                return true;
+            };
+
+            window.Add(weightButton);
+            buttons.Add(weightButton);
         }
 
         public override void Remove()
