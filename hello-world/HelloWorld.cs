80a317ff8e3db920554685b0a206c53200f193d7
Agnelo Vaz <agnelo.vaz@samsung.com>
2018-12-05 09:06:13 +0000

    DON'T MERGE HelloWorld showing TextLabel not resizing bug
    Change-Id: I02888b1c83c3a555b34e29ef6153a73044fad8fc

--- hello-world/HelloWorld.cs
+++ hello-world/HelloWorld.cs
@@ -1,65 +1,80 @@
-/*
- * Copyright (c) 2018 Samsung Electronics Co., Ltd.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
 using System;
+using System.Threading.Tasks;
 using Tizen.NUI;
-using Tizen.NUI.UIComponents;
 using Tizen.NUI.BaseComponents;
-using Tizen.NUI.Constants;
 
-class HelloWorldExample : NUIApplication
+namespace TextLabelSample
 {
-    /// <summary>
-    /// Override to create the required scene
-    /// </summary>
-    protected override void OnCreate()
+    class Program : NUIApplication
     {
-        // Up call to the Base class first
-        base.OnCreate();
+        TextLabel category;
+        string testtext = "test";
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+            test();
+        }
 
-        // Get the window instance and change background color
-        Window window = Window.Instance;
-        window.BackgroundColor = Color.White;
+        public async Task test()
+        {
+            Window.Instance.TouchEvent += Instance_TouchEvent;
+            View view = new View();
+            view.Size2D = new Size2D(720, 1280);
+            view.Position = new Position(0, 0, 0);
+            view.BackgroundColor = Color.Cyan;
+            Window.Instance.GetDefaultLayer().Add(view);
 
-        // Create a simple TextLabel
-        TextLabel title = new TextLabel("Hello World");
+            Window.Instance.BackgroundColor = Color.Red;
+            category = new TextLabel(testtext);
+            category.TextColor = Color.White;
+            category.PointSize = 19;
+            category.VerticalAlignment = VerticalAlignment.Center;
+            category.MultiLine = true;
+            category.Position = new Position(0, 0, 0);
+            category.BackgroundColor = Color.Blue;
 
-        // Ensure TextLabel matches its parent's size (i.e. Window size)
-        // By default, a TextLabel's natural size is the size of the text within it
-        title.LayoutWidthSpecification = ChildLayoutData.MatchParent;
-        title.LayoutHeightSpecification = ChildLayoutData.MatchParent;
+            category.Size2D = new Size2D(300,300);
+            category.WidthResizePolicy = ResizePolicyType.UseNaturalSize;
+            category.HeightResizePolicy = ResizePolicyType.UseNaturalSize;
+            category.TextColor = new Color(0.75f, 0.75f, 0.75f, 1.0f);
+            category.Opacity = 1.0f;
+            view.Add(category);
 
-        // By default, text is aligned to the top-left within the TextLabel
-        // Align it to the center of the TextLabel
-        title.HorizontalAlignment = HorizontalAlignment.Center;
-        title.VerticalAlignment = VerticalAlignment.Center;
+            //Timer timer = new Timer(1000);
+            //timer.Tick += Timer_Tick;
+            //timer.Start();
+        }
 
-        // Add the text to the window
-        window.Add(title);
-    }
+        private void Instance_TouchEvent(object sender, Window.TouchEventArgs e)
+        {
+            testtext += "a";
+            category.Text = testtext;
+            //ategory.SizeWidth = 200;
+            //category.SizeHeight = 500;
+            Tizen.Log.Error("MYLOG", "size width : " + category.SizeWidth + ", sizeHeight : " + category.SizeHeight);
+        }
 
-    /// <summary>
-    /// The main entry point for the application.
-    /// </summary>
-    [STAThread] // Forces app to use one thread to access NUI
-    static void Main(string[] args)
-    {
-        HelloWorldExample example = new HelloWorldExample();
-        example.Run(args);
+        private bool Timer_Tick(object source, Timer.TickEventArgs e)
+        {
+            testtext += "a";
+            category.Text = testtext;
+            Tizen.Log.Error("MYLOG", "size width : " + category.SizeWidth + ", sizeHeight : " + category.SizeHeight);
+            return true;
+        }
+
+        public void OnKeyEvent(object sender, Window.KeyEventArgs e)
+        {
+            if (e.Key.State == Key.StateType.Down && (e.Key.KeyPressedName == "XF86Back" || e.Key.KeyPressedName == "Escape"))
+            {
+                Exit();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var app = new Program();
+            app.Run(args);
+        }
     }
 }
 
