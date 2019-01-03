2a40751b4bdcb72d25c026fbbbafbb58158ee49a
Agnelo Vaz <agnelo.vaz@samsung.com>
2019-01-03 19:32:23 +0000

    Adding first Layouting NUnit test
    LayoutLength unit tests added
    
    Change-Id: I5205df643da46bf7f58234f35e77b50eb38082af

---
+++ unit-tests/LayoutingTests/LayoutLengthEx.Test.cs
@@ -0,0 +1,228 @@
+﻿using NUnit.Framework;
+using Tizen.NUI;
+using Tizen.NUI.BaseComponents;
+
+namespace LayoutingTests
+{
+  [TestFixture]
+  public class LayoutLengthExTest
+  {
+    [Test]
+    public void LayoutLengthEx_ConstructAndCompare()
+    {
+      var result = false;
+        int TEST_VALUE = 16;
+        LayoutLengthEx maxWidth = new LayoutLengthEx(TEST_VALUE);
+        if( maxWidth.AsRoundedValue() == TEST_VALUE )
+        {
+            result = true;
+        }
+
+      Assert.IsTrue(result, "LayoutLength not returning value set");
+    }
+
+    [Test]
+    public void LayoutLengthEx_DefaultConstructAndCompare()
+    {
+      var result = false;
+        float TEST_VALUE = 0.0f;
+        LayoutLengthEx maxWidth = new LayoutLengthEx(TEST_VALUE);
+        if( maxWidth.AsRoundedValue() == TEST_VALUE )
+        {
+            result = true;
+        }
+
+      Assert.IsTrue(result, "LayoutLength not returning default");
+    }
+
+    [Test]
+    public void LayoutLengthEx_ConstructAndCompareFloat()
+    {
+      var result = false;
+        float TEST_VALUE = 5.6f;
+        LayoutLengthEx maxWidth = new LayoutLengthEx(TEST_VALUE);
+        if( maxWidth.AsDecimal() == TEST_VALUE )
+        {
+            result = true;
+        }
+
+      Assert.IsTrue(result, "LayoutLength not returning default");
+    }
+
+    [Test]
+    public void LayoutLengthEx_ConstructAndReplace()
+    {
+        var result = false;
+        int ORIGINAL_TEST_VALUE = 16;
+        int REPLACEMENT_TEST_VALUE = 16;
+        LayoutLengthEx maxWidth = new LayoutLengthEx(ORIGINAL_TEST_VALUE);
+        maxWidth = new LayoutLengthEx(REPLACEMENT_TEST_VALUE);
+        if( maxWidth.AsRoundedValue() == REPLACEMENT_TEST_VALUE )
+        {
+            result = true;
+        }
+
+        Assert.IsTrue(result, "LayoutLength not returning replaced value");
+    }
+
+    [Test]
+    public void LayoutLengthEx_ConstructInt()
+    {
+        var result = false;
+        int ORIGINAL_TEST_VALUE = 16;
+        LayoutLengthEx maxWidth = new LayoutLengthEx(ORIGINAL_TEST_VALUE);
+        if( maxWidth.AsRoundedValue() == ORIGINAL_TEST_VALUE )
+        {
+            result = true;
+        }
+
+      Assert.IsTrue(result, "LayoutLength not returning rounded value that was set");
+    }
+
+    [Test]
+    public void LayoutLengthEx_ConstructFloat()
+    {
+        var result = false;
+        float ORIGINAL_TEST_VALUE = 16.4f;
+        float ROUNDED_TEST_VALUE = 16.0f;
+        LayoutLengthEx maxWidth = new LayoutLengthEx(ORIGINAL_TEST_VALUE);
+
+        if( maxWidth.AsRoundedValue() == ROUNDED_TEST_VALUE )
+        {
+            result = true;
+        }
+
+        Assert.IsTrue(result, "LayoutLength not returning rounded value that was set");
+    }
+
+    [Test]
+    public void LayoutLengthEx_Multiply()
+    {
+        var result = false;
+        float ORIGINAL_TEST_VALUE = 6.0f;
+        float ROUNDED_RESULT_VALUE = 36.0f;
+        LayoutLengthEx maxWidth = new LayoutLengthEx(ORIGINAL_TEST_VALUE) * new LayoutLengthEx(ORIGINAL_TEST_VALUE);
+
+        if( maxWidth.AsRoundedValue() == ROUNDED_RESULT_VALUE )
+        {
+            result = true;
+        }
+
+        Assert.IsTrue(result, "LayoutLength not returning multipied result");
+    }
+
+    [Test]
+    public void LayoutLengthEx_MultiplyByInt()
+    {
+        var result = false;
+        float ORIGINAL_TEST_VALUE = 6.0f;
+        float ROUNDED_RESULT_VALUE = 18.0f;
+        LayoutLengthEx maxWidth = new LayoutLengthEx(ORIGINAL_TEST_VALUE) * 3;
+
+        if( maxWidth.AsRoundedValue() == ROUNDED_RESULT_VALUE )
+        {
+            result = true;
+        }
+
+        Assert.IsTrue(result, "LayoutLength not returning multipied result");
+    }
+
+    [Test]
+    public void LayoutLengthEx_Divide()
+    {
+        var result = false;
+        float ORIGINAL_TEST_VALUE = 18.0f;
+        float DENOMINATOR_VALUE = 3.0f;
+        float ROUNDED_RESULT_VALUE = 6.0f;
+        LayoutLengthEx maxWidth = new LayoutLengthEx(ORIGINAL_TEST_VALUE) / new LayoutLengthEx(DENOMINATOR_VALUE);
+
+        if( maxWidth.AsRoundedValue() == ROUNDED_RESULT_VALUE )
+        {
+            result = true;
+        }
+
+        Assert.IsTrue(result, "LayoutLength not returning divided result");
+    }
+
+    [Test]
+    public void LayoutLengthEx_DividedByInt()
+    {
+        var result = false;
+        float ORIGINAL_TEST_VALUE = 18.0f;
+        float ROUNDED_RESULT_VALUE = 6.0f;
+        LayoutLengthEx maxWidth = new LayoutLengthEx(ORIGINAL_TEST_VALUE) / 3;
+
+        if( maxWidth.AsRoundedValue() == ROUNDED_RESULT_VALUE )
+        {
+            result = true;
+        }
+
+        Assert.IsTrue(result, "LayoutLength not returning divided result");
+    }
+
+    [Test]
+    public void LayoutLengthEx_Subtraction()
+    {
+        var result = false;
+        float ORIGINAL_TEST_VALUE = 18.0f;
+        float SUBTRACT_VALUE = 6.0f;
+        float ROUNDED_RESULT_VALUE = 12.0f;
+        LayoutLengthEx maxWidth = new LayoutLengthEx(ORIGINAL_TEST_VALUE) - new LayoutLengthEx(SUBTRACT_VALUE);
+
+        if( maxWidth.AsRoundedValue() == ROUNDED_RESULT_VALUE )
+        {
+            result = true;
+        }
+
+        Assert.IsTrue(result, "LayoutLength not returning subtracted result");
+    }
+
+    [Test]
+    public void LayoutLengthEx_Addition()
+    {
+        var result = false;
+        float ORIGINAL_TEST_VALUE = 3.0f;
+        float ROUNDED_RESULT_VALUE = 6.0f;
+        LayoutLengthEx maxWidth = new LayoutLengthEx(ORIGINAL_TEST_VALUE) + new LayoutLengthEx(ORIGINAL_TEST_VALUE);
+
+        if( maxWidth.AsRoundedValue() == ROUNDED_RESULT_VALUE )
+        {
+            result = true;
+        }
+
+        Assert.IsTrue(result, "LayoutLength not returning added result");
+    }
+
+    [Test]
+    public void LayoutLengthEx_Int_Addition()
+    {
+        var result = false;
+        float ORIGINAL_TEST_VALUE = 3.0f;
+        float ROUNDED_RESULT_VALUE = 6.0f;
+        LayoutLengthEx maxWidth = new LayoutLengthEx(ORIGINAL_TEST_VALUE) + 3;
+
+        if( maxWidth.AsRoundedValue() == ROUNDED_RESULT_VALUE )
+        {
+            result = true;
+        }
+
+        Assert.IsTrue(result, "LayoutLength not returning added result");
+    }
+
+    [Test]
+    public void LayoutLengthEx_Int_Subtraction()
+    {
+        var result = false;
+        float ORIGINAL_TEST_VALUE = 18.0f;
+        float ROUNDED_RESULT_VALUE = 12.0f;
+        LayoutLengthEx maxWidth = new LayoutLengthEx(ORIGINAL_TEST_VALUE) - 6;
+
+        if( maxWidth.AsRoundedValue() == ROUNDED_RESULT_VALUE )
+        {
+            result = true;
+        }
+
+        Assert.IsTrue(result, "LayoutLength not returning subtracted result");
+    }
+  }
+}
---
+++ unit-tests/LayoutingTests/MeasureSpecification.Test.cs
@@ -0,0 +1,76 @@
+﻿using NUnit.Framework;
+using Tizen.NUI;
+using Tizen.NUI.BaseComponents;
+
+namespace LayoutingTests
+{
+  [TestFixture]
+  public class MeasureSpecificationTest
+  {
+    [Test]
+    public void MeasureSpecification_ConstructAndCompare()
+    {
+        var result = false;
+        LayoutLengthEx TEST_VALUE1 = new LayoutLengthEx(16);
+        MeasureSpecification.ModeType TEST_VALUE2 = MeasureSpecification.ModeType.Exactly;
+
+        MeasureSpecification measureSpec = new MeasureSpecification(TEST_VALUE1, TEST_VALUE2);
+        if((measureSpec.Size.Equals(TEST_VALUE1)) && (measureSpec.Mode == TEST_VALUE2))
+        {
+            result = true;
+        }
+
+        Assert.IsTrue(result, "MeasureSpecification not returning values set");
+    }
+
+    [Test]
+    public void MeasureSpecification_Defaults()
+    {
+        var result = false;
+        LayoutLengthEx TEST_VALUE1 = new LayoutLengthEx();
+
+        MeasureSpecification measureSpec = new MeasureSpecification();
+
+        if((measureSpec.Size.Equals(TEST_VALUE1)) && (measureSpec.Mode == MeasureSpecification.ModeType.Unspecified))
+        {
+            result = true;
+        }
+
+        Assert.IsTrue(result, "MeasureSpecification defaults failed to match:" + TEST_VALUE1.AsRoundedValue());
+    }
+
+    [Test]
+    public void MeasureSpecification_OperatorCompare()
+    {
+        var result = false;
+        LayoutLengthEx TEST_VALUE1 = new LayoutLengthEx(14);
+
+        MeasureSpecification measureSpec = new MeasureSpecification(TEST_VALUE1,MeasureSpecification.ModeType.Unspecified );
+
+        if((measureSpec.Size == TEST_VALUE1) && (measureSpec.Mode == MeasureSpecification.ModeType.Unspecified))
+        {
+            result = true;
+        }
+
+        Assert.IsTrue(result, "MeasureSpecification defaults failed to match:" + TEST_VALUE1.AsRoundedValue());
+    }
+
+    [Test]
+    public void MeasureSpecification_EqualsCompare()
+    {
+        var result = false;
+        LayoutLengthEx TEST_VALUE1 = new LayoutLengthEx(14);
+
+        MeasureSpecification measureSpec = new MeasureSpecification(TEST_VALUE1,MeasureSpecification.ModeType.Unspecified );
+
+        if((measureSpec.Size.Equals(TEST_VALUE1)) && (measureSpec.Mode == MeasureSpecification.ModeType.Unspecified))
+        {
+            result = true;
+        }
+
+        Assert.IsTrue(result, "MeasureSpecification defaults failed to match:" + TEST_VALUE1.AsRoundedValue());
+    }
+
+  }
+} // namespace LayoutingTests
+
