3044ffee5455b6bed107f5b974d0bf96a77596fc
Agnelo Vaz <agnelo.vaz@samsung.com>
2019-01-03 19:32:23 +0000

    Adding first Layouting NUnit test
    Change-Id: I5205df643da46bf7f58234f35e77b50eb38082af

---
+++ unit-tests/LayoutingTests/LayoutController.Test.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using Tizen.NUI;
+using Tizen.NUI.BaseComponents;
+
+namespace LayoutingTests
+{
+    [TestFixture]
+    public class TestOne
+    {
+	[Test]
+ 	public void HasControllerBeenCreated()
+ 	{
+	   var result = false;
+       const int TEST_VALUE = 16;
+       LayoutLength maxWidth = new LayoutLength(TEST_VALUE);
+       if( maxWidth.Value == TEST_VALUE )
+       {
+           result = true;
+       }
+
+	   Assert.IsTrue(result, "1 should not be prime");
+	}
+    }
+}
