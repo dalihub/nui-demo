4b424b9577ef92569e268128e777cff2cfe663d4
Agnelo Vaz <agnelo.vaz@samsung.com>
2019-02-26 19:35:38 +0000

    LinearLayoutEx demo
    Change-Id: If9c503e608649d5576a4789cd2f790b1bb9b312c

---
+++ layout-demo/LinearExampleEx.cs
@@ -0,0 +1,117 @@
+using System;
+using System.Collections.Generic;
+using Tizen.NUI;
+using Tizen.NUI.BaseComponents;
+using Tizen.NUI.UIComponents;
+
+namespace LayoutDemo
+{
+    class LinearExampleEx : Example
+    {
+        public LinearExampleEx() : base( "LinearLayoutEx" )
+        {}
+
+        static class TestImages
+        {
+            private const string resources = "./res";
+
+            /// Child image filenames
+            public static readonly string[] s_images = new string[]
+            {
+                resources + "/images/application-icon-101.png",
+                resources + "/images/application-icon-102.png",
+                resources + "/images/application-icon-103.png",
+                resources + "/images/application-icon-104.png"
+            };
+        }
+
+        private View view;
+        private List<PushButton> buttons = new List<PushButton>();
+
+        public override void Create()
+        {
+            view = new View();
+            view.Name = "LinearExample";
+            view.ParentOrigin = ParentOrigin.Center;
+            view.PivotPoint = PivotPoint.Center;
+            view.PositionUsesPivotPoint = true;
+            view.WidthSpecification = 480;
+            view.HeightSpecification = 800;
+
+            var layout = new LinearLayoutEx();
+            view.LayoutEx = layout;
+            view.LayoutDirection = ViewLayoutDirectionType.LTR;
+
+            // Add child image-views to the created view
+            foreach (String image in TestImages.s_images)
+            {
+                ImageView imageView = LayoutingExample.CreateChildImageView(image, new Size2D(100, 100));
+                view.Add(imageView);
+            }
+
+            LayoutingExample.GetWindowView().Add(view);
+
+            PushButton directionButton = new PushButton();
+            LayoutingExample.SetUnselectedIcon(directionButton, "./res/images/icon-reverse.png");
+            LayoutingExample.SetSelectedIcon(directionButton, "./res/images/icon-reverse-selected.png");
+            directionButton.ParentOrigin = new Vector3(0.33f, 1.0f, 0.5f);
+            directionButton.PivotPoint = PivotPoint.BottomCenter;
+            directionButton.PositionUsesPivotPoint = true;
+            directionButton.MinimumSize = new Vector2(75, 75);
+            directionButton.Clicked += (sender, e) =>
+            {
+                if (this.view.LayoutDirection == ViewLayoutDirectionType.LTR)
+                {
+                    this.view.LayoutDirection = ViewLayoutDirectionType.RTL;
+                    LayoutingExample.SetUnselectedIcon(directionButton, "./res/images/icon-play.png");
+                    LayoutingExample.SetUnselectedIcon(directionButton, "./res/images/icon-play-selected.png");
+                }
+                else
+                {
+                    this.view.LayoutDirection = ViewLayoutDirectionType.LTR;
+                    LayoutingExample.SetUnselectedIcon(directionButton, "./res/images/icon-reverse.png");
+                    LayoutingExample.SetSelectedIcon(directionButton, "./res/images/icon-reverse-selected.png");
+                }
+                return true;
+            };
+            LayoutingExample.GetWindowView().Add(directionButton);
+            buttons.Add(directionButton);
+
+            PushButton rotateButton = new PushButton();
+            LayoutingExample.SetUnselectedIcon(rotateButton, "./res/images/icon-reset.png");
+            LayoutingExample.SetSelectedIcon(rotateButton, "./res/images/icon-reset-selected.png");
+            rotateButton.ParentOrigin = new Vector3(0.66f, 1.0f, 0.5f);
+            rotateButton.PivotPoint = PivotPoint.BottomCenter;
+            rotateButton.PositionUsesPivotPoint = true;
+            rotateButton.MinimumSize = new Vector2(75, 75);
+            rotateButton.Clicked += (sender, e) =>
+            {
+                LinearLayoutEx linearLayout = (LinearLayoutEx)this.view.LayoutEx;
+                if (linearLayout.LinearOrientation == LinearLayoutEx.Orientation.Horizontal)
+                {
+                    linearLayout.LinearOrientation = LinearLayoutEx.Orientation.Vertical;
+                }
+                else
+                {
+                    linearLayout.LinearOrientation = LinearLayoutEx.Orientation.Horizontal;
+                }
+                return true;
+            };
+
+            LayoutingExample.GetWindowView().Add(rotateButton);
+            buttons.Add(rotateButton);
+        }
+
+        public override void Remove()
+        {
+            LayoutingExample.GetWindowView().Remove(view);
+
+            view = null;
+            foreach (PushButton button in buttons)
+            {
+                LayoutingExample.GetWindowView().Remove(button);
+            }
+            buttons.Clear();
+        }
+    };
+}
