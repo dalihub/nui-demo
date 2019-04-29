6b4865d44671a9020c33954a825684a5ba356479
Adeel Kazmi <adeel.kazmi@samsung.com>
2019-04-29 10:54:17 +0100

    Added debug output to simple-text
    Change-Id: I9474a1031c03301426261e43916964cbef2baf05

--- simple-text/simple-text.cs
+++ simple-text/simple-text.cs
@@ -1,5 +1,5 @@
 /*
-* Copyright (c) 2017 Samsung Electronics Co., Ltd.
+* Copyright (c) 2019 Samsung Electronics Co., Ltd.
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -334,6 +334,8 @@ namespace HelloWorldTest
         [STAThread]
         static void Main(string[] args)
         {
+            // Do not remove this print out - helps with the TizenFX stub sync issue
+            Console.WriteLine("Running Example...");
             Example example = new Example();
             example.Run(args);
         }
