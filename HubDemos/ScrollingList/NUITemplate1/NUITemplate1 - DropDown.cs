9a238f865a7cded804a3e5f650e9765a06b611eb
agnelo <vaz.agnelo@gmail.com>
2019-12-04 21:07:14 +0900

    ScrollingList example

---
+++ HubDemos/ScrollingList/NUITemplate1/NUITemplate1 - DropDown.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using Tizen.NUI;
+using Tizen.NUI.BaseComponents;
+using Tizen.NUI.Components;
+
+namespace DropDown1
+{
+    class Program : NUIApplication
+    {
+        static class TestImages
+        {
+            private const string resources = "./res";
+
+            /// Child image filenames
+            public static readonly string[] images = new string[]
+            {
+                "/images/dropdown_bg.png",
+                "/images/dropdown_checkbox_on.png",
+                "/images/list_ic_dropdown.png",
+            };
+        }
+
+
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+            Initialize();
+        }
+
+        void Initialize()
+        {
+            Window.Instance.BackgroundColor = Color.White;
+            Size tenthOfWindowSize = new Size(Window.Instance.WindowSize.Width * .1f, Window.Instance.WindowSize.Height * .1f);
+
+            LinearLayout linearLayout = new LinearLayout();
+            linearLayout.LinearOrientation = LinearLayout.Orientation.Vertical;
+            View root = new View()
+            {
+                Name = "example-root",
+                WidthSpecification = LayoutParamPolicies.MatchParent,
+                HeightSpecification = LayoutParamPolicies.WrapContent,
+                Padding = new Extents((ushort)tenthOfWindowSize.Width, (ushort)tenthOfWindowSize.Width, (ushort)(tenthOfWindowSize.Height), 0),
+                //Layout = linearLayout,
+            };
+
+            // Don't use root whilst testing DropDown style refactor, add to Window instead.
+            //Window.Instance.Add(root);
+
+            TextLabel text = new TextLabel()
+            {
+                Text = "DropDown Clicked item string is ",
+                PointSize = 18,
+                HeightSpecification = 80,
+                WidthSpecification = LayoutParamPolicies.MatchParent,
+                HorizontalAlignment = HorizontalAlignment.Center,
+                MultiLine = true,
+                BackgroundColor = new Color(0.8f, 0.8f, 0.8f, 1.0f),
+            };
+            // Don't use root whilst testing DropDown style refactor, add to Window instead.
+            //            root.Add(text);
+            Window.Instance.Add(text);
+
+            DropDown dropDown = new DropDown();
+            dropDown.Size2D = new Size2D(900, 108);
+            dropDown.Position2D = new Position2D(50, 300);
+            dropDown.Style.HeaderText.Text = "TitleArea";
+            dropDown.Style.HeaderText.TextColor = new Color(0, 0, 0, 1);
+            dropDown.Style.HeaderText.PointSize = 28;
+            dropDown.Style.HeaderText.FontFamily = "SamsungOneUI 500C";
+            dropDown.Style.Button.Text.Text = "DropDown Text";
+            dropDown.Style.Button.Text.TextColor = new Color(0, 0, 0, 1);
+            dropDown.Style.Button.Text.PointSize = 20;
+            dropDown.Style.Button.Text.FontFamily = "SamsungOneUI 500";
+            dropDown.Style.Button.Icon.ResourceUrl = this.DirectoryInfo.Resource + TestImages.images[2];
+            dropDown.Style.Button.Icon.Size = new Size(48, 48);
+            dropDown.Style.Button.BackgroundColor.Pressed = new Color(0, 1, 0, 0.4f);
+            dropDown.Style.Button.BackgroundColor.Normal = new Color(0, 0, 1, 0.4f);
+            dropDown.Style.Button.Size = new Size(300, 60);
+            dropDown.Space.Start = 56;
+            dropDown.SpaceBetweenButtonTextAndIcon = 8;
+            dropDown.Style.ListBackgroundImage.ResourceUrl = this.DirectoryInfo.Resource + TestImages.images[0];
+            dropDown.Style.ListBackgroundImage.Border = new Rectangle(51, 51, 51, 51);
+            dropDown.Style.ListBackgroundImage.BackgroundColor = new Color(1, 1, 1, 1f);
+            dropDown.ListMargin.Start = 20;
+            dropDown.ListMargin.Top = 20;
+            dropDown.BackgroundColor = new Color(1, 1, 1, 1);
+            dropDown.ListSize = new Size(360, 500);
+            dropDown.ListPadding = new Extents(4, 4, 4, 4);
+
+            // Don't use root whilst testing DropDown style refactor, add to Window instead.
+            //root.Add(dropDown);
+            Window.Instance.Add(dropDown);
+
+            for (int i = 0; i < 8; i++)
+            {
+                DropDown.DropDownDataItem item = new DropDown.DropDownDataItem();
+                item.Size = new Size(360, 96);
+                item.BackgroundColorSelector = new Selector<Color>
+                {
+                    Pressed = new Color(0, 0, 0, 0.4f),
+                    Other = new Color(1, 1, 1, 0),
+                };
+                item.Text = "Normal list " + i;
+                item.PointSize = 18;
+                item.FontFamily = "SamsungOne 500";
+                item.TextPosition = new Position(28, 0);
+                item.CheckImageSize = new Size(40, 40);
+                item.CheckImageResourceUrl = this.DirectoryInfo.Resource + TestImages.images[1];
+                item.CheckImageGapToBoundary = 16;
+                dropDown.AddItem(item);
+            }
+
+            dropDown.SelectedItemIndex = 2;
+
+            dropDown.RaiseToTop();
+            Window.Instance.KeyEvent += OnKeyEvent;
+        }
+
+        public void OnKeyEvent(object sender, Window.KeyEventArgs e)
+        {
+            if (e.Key.State == Key.StateType.Down && (e.Key.KeyPressedName == "XF86Back" || e.Key.KeyPressedName == "Escape"))
+            {
+                Exit();
+            }
+        }
+
+    //    static void Main(string[] args)
+      //  {
+        //    var app = new Program();
+          //  app.Run(args);
+       // }
+    }
+}
---
+++ HubDemos/ScrollingList/NUITemplate1/NUITemplate1.cs
@@ -0,0 +1,204 @@
+﻿using System.IO;
+using Tizen.NUI;
+using Tizen.NUI.BaseComponents;
+using Tizen.NUI.Components;
+
+namespace NUITemplate1
+{
+    class Program : NUIApplication
+    {
+        static class TestImages
+        {
+            public const string listIconsDir = "/images/applicationIcons";
+
+            /// Child image filenames
+            public static readonly string[] iconImage = new string[]
+            {
+                "/images/dropdown_bg.png",
+                "/images/toggle-selected.jpg",
+                "/images/toggle-normal.jpg",
+            };
+
+        }
+
+        private readonly string[] applicationNames = new string[]
+        {
+            "Dash", "Music", "Calendar", "Clock", "Deals", "Engineer Mode", "FridgeManager","Gallery",
+            "FoodHub", "HomeHub", "Internet", "Planner", "Memo", "AM Brief", "Demo", "Charm",
+            "Circle", "Care","Settings", "Shopping List", "Recipes", "SmartView", "SmartThings",
+            "Streaming", "Timer", "Todo", "Trivia", "Radio"
+        };
+
+        private string[] applicationIconsArray;
+
+        private View root = null;
+        private View listViewContainer = null;
+
+        private LayoutScroller layoutScroller = null;
+
+        protected override void OnCreate()
+        {
+            Window.Instance.BackgroundColor = Color.White;
+            float rootWidth = Window.Instance.WindowSize.Width;
+            float rootHeight = Window.Instance.WindowSize.Height;
+            root = new View()
+            {
+                Size = new Size(rootWidth, rootHeight, 0.0f),
+            };
+            Window.Instance.Add(root);
+
+            CreateList();
+            PopulateList();
+
+            Window.Instance.KeyEvent += OnKeyEvent;
+        }
+
+        public void Deactivate()
+        {
+            if (root != null)
+            {
+                root.Dispose();
+            }
+        }
+
+        private void GetFiles(string sourceDirectory, ref string[] targetArray)
+        {
+            targetArray = Directory.GetFiles(sourceDirectory);
+        }
+
+        private void CreateList()
+        {
+
+            LinearLayout linear = new LinearLayout();
+            linear.LinearOrientation = LinearLayout.Orientation.Vertical;
+
+            listViewContainer = new View()
+            {
+                Layout = linear,
+                Name = "DropDownMenuList",
+                WidthSpecification = LayoutParamPolicies.MatchParent,
+                HeightSpecification = LayoutParamPolicies.MatchParent,
+                Focusable = true,
+                Padding = new Extents(40,40,0,0),
+            };
+
+            TransitionComponents slowPositioning = new TransitionComponents();
+            slowPositioning.AlphaFunction = new AlphaFunction(AlphaFunction.BuiltinFunctions.Linear);
+            slowPositioning.Delay = 0;
+            slowPositioning.Duration = 164;
+
+            listViewContainer.LayoutTransition = new LayoutTransition(TransitionCondition.LayoutChanged,
+                                                                      AnimatableProperties.Position,
+                                                                      0.0,
+                                                                      slowPositioning);
+
+            listViewContainer.LayoutTransition = new LayoutTransition(TransitionCondition.LayoutChanged,
+                                                                      AnimatableProperties.Size,
+                                                                      0.0,
+                                                                      slowPositioning);
+
+            layoutScroller = new LayoutScroller()
+            {
+                Name = "LayoutScroller",
+            };
+            layoutScroller.AddLayoutToScroll(listViewContainer);
+
+            layoutScroller.WidthSpecification = LayoutParamPolicies.MatchParent;
+            layoutScroller.HeightSpecification = LayoutParamPolicies.MatchParent;
+
+            root.Add(layoutScroller);
+        }
+
+        private View CreateListItem(int imageIndex, string text, bool toggle)
+        {
+            LinearLayout linearLayout = new LinearLayout()
+            {
+                LinearOrientation = LinearLayout.Orientation.Horizontal,
+            };
+
+            View listItem = new View()
+            {
+                WidthSpecification = LayoutParamPolicies.MatchParent,
+                HeightSpecification = LayoutParamPolicies.WrapContent,
+                Layout = linearLayout,
+                Padding = new Extents(5, 5, 55, 55),
+                Name = "flexibleLayout-entry-" + text,
+            };
+
+            TextLabel textLabel = new TextLabel()
+            {
+                Text = text,
+                Name = "flexibleLayout-text-label-" + text,
+                Margin = new Extents(90, 0, 0, 0),
+                TextColor = new Color(0.6f,0.6f,0.6f,1),
+            };
+
+            if (imageIndex>=0)
+            {
+                ImageView icon = new ImageView(applicationIconsArray[imageIndex]);
+                listItem.Add(icon);
+            }
+
+            listItem.Add(textLabel);
+
+            if (toggle)
+            {
+                Tizen.NUI.Components.ButtonStyle buttonStyle = new Tizen.NUI.Components.ButtonStyle
+                {
+                    Icon = new ImageViewStyle
+                    {
+                        ResourceUrl = new Tizen.NUI.Components.StringSelector
+                        {
+                            Normal = this.DirectoryInfo.Resource + TestImages.iconImage[2],
+                            Selected = this.DirectoryInfo.Resource + TestImages.iconImage[1]
+                        },
+                    },
+                    IsSelectable = true,
+                };
+
+                Tizen.NUI.Components.Button toggleButton = new Tizen.NUI.Components.Button(buttonStyle)
+                {
+                    Name = "toggleButton",
+                    Size2D = new Size2D(90, 30),
+                };
+
+                listItem.Add(toggleButton);
+            }
+
+
+            return listItem;
+        }
+
+        private void PopulateList()
+        {
+            GetFiles(this.DirectoryInfo.Resource+TestImages.listIconsDir, ref applicationIconsArray);
+            int numberOfIcons = applicationIconsArray.Length;
+            var rand = new System.Random();
+
+            foreach (string name in applicationNames)
+            {
+
+                listViewContainer.Add(CreateListItem(rand.Next(0,numberOfIcons), name, false));
+                listViewContainer.Add(new View() { WidthSpecification = LayoutParamPolicies.MatchParent,
+                                                   HeightSpecification = 2,
+                                                   BackgroundColor = new Color(0.8f,0.8f,0.8f,1),
+                                                 });
+            };
+
+        }
+
+        public void OnKeyEvent(object sender, Window.KeyEventArgs e)
+        {
+            if (e.Key.State == Key.StateType.Down && (e.Key.KeyPressedName == "XF86Back" || e.Key.KeyPressedName == "Escape"))
+            {
+                Exit();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var app = new Program();
+            app.Run(args);
+        }
+    }
+}
