33080378e1396e79858c471df8662e25868e4a3b
neostom432 <minho.sun@samsung.com>
2020-04-24 17:49:44 +0900

    WearableList Sample
    Two samples
    
    1. Simple sample to demostrate how to use WearableList
    2. Complicate application which uses WearableList and BlurView

---
+++ wearable-samples/MessageApplication/MenuList.cs
@@ -0,0 +1,116 @@
+using System;
+using System.Collections.Generic;
+using Tizen.NUI;
+using Tizen.NUI.BaseComponents;
+using Tizen.NUI.Components;
+using Tizen.NUI.Wearable;
+
+namespace WearableSample
+{
+    class MenuItem : RecycleItem
+    {
+        public MenuItem()
+        {
+            Size = new Size(360, 108);
+
+            BackgroundView = new View()
+            {
+                CornerRadius = 54,
+                BackgroundColor = new Color("#FFFFFF0F"),
+                Opacity = 0.0f,
+                Size = new Size(360, 108),
+            };
+
+            View content = new View()
+            {
+                WidthSpecification = LayoutParamPolicies.MatchParent,
+                HeightSpecification = LayoutParamPolicies.MatchParent,
+                Layout = new LinearLayout()
+                {
+                    LinearOrientation = LinearLayout.Orientation.Horizontal,
+                    LinearAlignment = LinearLayout.Alignment.CenterVertical,
+                },
+                Padding = new Extents(16, 30, 0, 0)
+            };
+
+            Add(BackgroundView);
+            Add(content);
+
+            Icon = new ImageView()
+            {
+                Size = new Size(46, 46),
+                Margin = new Extents(23, 31, 0, 0),
+                Color = Color.White,
+            };
+
+            Title = new TextLabel()
+            {
+                PixelSize = 38.0f,
+                Weight = 1,
+                VerticalAlignment = VerticalAlignment.Center,
+                HorizontalAlignment = HorizontalAlignment.Center,
+                TextColor = Color.White,
+            };
+
+            content.Add(Icon);
+            content.Add(Title);
+        }
+
+        public override void OnFocusGained()
+        {
+            BackgroundView.Opacity = 1.0f;
+        }
+
+        public override void OnFocusLost()
+        {
+            BackgroundView.Opacity = 0.0f;
+        }
+
+        public View BackgroundView { get; set; }
+        public ImageView Icon { get; set; }
+        public TextLabel Title { get; set; }
+    }
+
+    class MenuListAdapter : RecycleAdapter
+    {
+        public MenuListAdapter()
+        {
+
+        }
+
+        public override RecycleItem CreateRecycleItem()
+        {
+            return new MenuItem();
+        }
+
+        public override void BindData(RecycleItem item)
+        {
+            MenuItem target = item as MenuItem;
+            MenuData data = Data[item.DataIndex] as MenuData;
+
+            target.Title.Text = data.Title;
+            target.Icon.ResourceUrl = data.ResourceUrl;
+        }
+    }
+
+    class MenuData
+    {
+        public string Title { get; set; }
+        public string ResourceUrl { get; set; }
+    }
+
+
+    class MenuList : WearableList
+    {
+        public MenuList() : base(new MenuListAdapter()
+        {
+            Data = new List<object>{
+            new MenuData(){Title = "Write", ResourceUrl = "./res/images/send_message.png"},
+            new MenuData(){Title = "Delete", ResourceUrl = "./res/images/delete.png"},
+            new MenuData(){Title = "Settings", ResourceUrl = "./res/images/settings.png"}
+        }
+        })
+        {
+        }
+    }
+}
