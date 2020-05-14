02d09acfacafdc580fae39f31f02a077127121f7
Jiyun Yang <ji.yang@samsung.com>
2020-05-14 17:55:49 +0900

    [CircularScrollBar] Update example
    Signed-off-by: Jiyun Yang <ji.yang@samsung.com>

--- wearable-samples/List/CircularScrollBar/CircularScrollBarExample.cs
+++ wearable-samples/List/CircularScrollBar/CircularScrollBarExample.cs
@@ -16,16 +16,17 @@
  */
 using System;
 using Tizen.NUI;
+using Tizen.NUI.BaseComponents;
 using Tizen.NUI.Wearable;
 
 public class ComponentExample : NUIApplication
 {
-    CircularScrollBar scrollbar;
+    const float contentLength = 1000;
+    const float speed = 10;
+
+    float screenSize;
     float currentPosition = 0;
-    float contentLength = 800;
-    float lastY = 0;
-    Color normalThumbColor;
-    Color touchedThumbColor = new Color(0.0f, 0.549f, 1.0f, 1.0f);
+    CircularScrollBar scrollbar;
 
     public ComponentExample() : base(new Size2D(360, 360), new Position2D(0, 0))
     {
@@ -36,9 +37,9 @@ public class ComponentExample : NUIApplication
         base.OnCreate();
 
         var window = NUIApplication.GetDefaultWindow();
-        var fullSize = Math.Min(window.Size.Width, window.Size.Height) - 1;
+        screenSize = Math.Min(window.Size.Width, window.Size.Height) - 1;
 
-        scrollbar = new CircularScrollBar(contentLength, fullSize, currentPosition)
+        scrollbar = new CircularScrollBar(contentLength, screenSize, currentPosition)
         {
             ParentOrigin = ParentOrigin.Center,
             PivotPoint = PivotPoint.Center,
@@ -46,42 +47,18 @@ public class ComponentExample : NUIApplication
         };
 
         window.Add(scrollbar);
-        window.TouchEvent += OnTouch;
-
-        normalThumbColor = scrollbar.ThumbColor;
+        scrollbar.TouchEvent += OnTouch;
     }
 
-    void OnTouch(object target, Window.TouchEventArgs args)
+    bool OnTouch(object target, View.TouchEventArgs args)
     {
-        var currentY = args.Touch.GetScreenPosition(0).Y;
-
         if (args.Touch.GetState(0) == PointStateType.Down)
         {
-            scrollbar.ThumbColor = touchedThumbColor;
-        }
-        else if (args.Touch.GetState(0) == PointStateType.Up)
-        {
-            scrollbar.ThumbColor = new Color(0.6f, 0.6f, 0.6f, 1.0f);
-        }
-        else if (args.Touch.GetState(0) == PointStateType.Motion)
-        {
-            var nextPosition = currentPosition + currentY - lastY;
-
-            if (nextPosition < 0)
-            {
-                nextPosition = 0;
-            }
-            else if (nextPosition > contentLength)
-            {
-                nextPosition = contentLength;
-            }
-
-            scrollbar.ScrollTo(nextPosition);
-
-            currentPosition = nextPosition;
+            currentPosition = currentPosition == 0 ? contentLength : 0;
+            scrollbar.ScrollTo(currentPosition, 500);
         }
 
-        lastY = currentY;
+        return true;
     }
 
     static void Main(string[] args)
